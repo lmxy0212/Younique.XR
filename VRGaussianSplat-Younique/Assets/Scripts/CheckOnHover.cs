a59ee0c1a50aef03b188629777b656e2f0e700de
Manxueying Li <manxueying.li@columbia.edu>
2024-04-09 19:25:51 -0400

    BOOK FLIPPPP

--- VRGaussianSplat-Younique/Assets/Scripts/CheckOnHover.cs
+++ VRGaussianSplat-Younique/Assets/Scripts/CheckOnHover.cs
@@ -4,10 +4,13 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class CheckOnHover : MonoBehaviour
 {
     private UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable interactable;
+    private Animator animator; // Reference to the Animator component
+    public bool isTouching;
 
     private void Awake()
     {
         interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable>();
+        animator = GetComponent<Animator>(); // Get the Animator component
 
         interactable.hoverEntered.AddListener(OnHoverEntered);
         interactable.hoverExited.AddListener(OnHoverExited);
@@ -22,11 +25,21 @@ public class CheckOnHover : MonoBehaviour
     private void OnHoverEntered(HoverEnterEventArgs arg)
     {
         Debug.Log($"{arg.interactableObject.transform.name} is being touched.");
-
+        isTouching = true;
+        PlayPageFlipAnimation();
     }
 
     private void OnHoverExited(HoverExitEventArgs arg)
     {
         Debug.Log($"{arg.interactableObject.transform.name} stopped being touched.");
+        isTouching = false;
+    }
+
+    private void PlayPageFlipAnimation()
+    {
+        if (animator != null)
+        {
+            animator.Play("PageFlip", 0, 0); // Play the PageFlip animation from the start
+        }
     }
 }
