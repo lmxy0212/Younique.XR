9aa012f73ac68860fc072fbfc47aa75b4d39a7fb
lmxy-mbp <lmxyngnl@gmail.com>
2024-04-15 12:50:42 -0400

    mute audio for 3s on play

--- VRGaussianSplat-Younique/Assets/Scripts/PlayAudio.cs
+++ VRGaussianSplat-Younique/Assets/Scripts/PlayAudio.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 public class PlayAudio : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip clip;
     public float volume = 1.0f;
-
+    public float muteDuration = 3.0f;
+    private float timer;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-
+        audioSource.volume = 0f;
+        timer = muteDuration;
     }
-
 
     void Update()
     {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            audioSource.volume = volume;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
